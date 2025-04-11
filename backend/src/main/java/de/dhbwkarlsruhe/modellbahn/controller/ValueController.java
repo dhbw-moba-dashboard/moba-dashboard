5cdcf979911b32e487a47c0f25d0ea7cf94a52f2
traible <traible@apollon.de>
2025-04-11 20:42:51 +0200

    Fixed the server not starting by improving the declaration of a method

--- backend/src/main/java/de/dhbwkarlsruhe/modellbahn/controller/ValueController.java
+++ backend/src/main/java/de/dhbwkarlsruhe/modellbahn/controller/ValueController.java
@@ -11,26 +11,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
-public class ValueController
-{
+public class ValueController {
     private final ValueService valueService;
 
     @GetMapping("/request/speed/{locID}")
-    public ResponseEntity<List<String>> getLocSpeed(@PathVariable int locID, @RequestParam int startTime, @RequestParam int endTime)
-    {
-        List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.SPEED, locID, startTime, endTime);
-        List<String> jsonRepresentation = valueList.stream()
-                .map(Value::toString)
-                .toList();
-        return ResponseEntity.ok(jsonRepresentation);
+    public ResponseEntity<List<String>> getLocSpeed(
+            @PathVariable int locID,
+            @RequestParam(required = false) Optional<Integer> startTime,
+            @RequestParam(required = false) Optional<Integer> endTime,
+            @RequestParam(required = false) Optional<Integer> entries
+    ) {
+        if (startTime.isPresent() && endTime.isPresent()) {
+            List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.SPEED, locID, startTime.get(), endTime.get());
+            List<String> jsonRepresentation = valueList.stream()
+                    .map(Value::toString)
+                    .toList();
+            return ResponseEntity.ok(jsonRepresentation);
+        }
+
+        if(entries.isPresent()) {
+            List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.SPEED, locID, entries.get());
+            List<String> jsonRepresentation = valueList.stream()
+                    .map(Value::toString)
+                    .toList();
+            return ResponseEntity.ok(jsonRepresentation);
+        }
+
+        return ResponseEntity.badRequest().build();
     }
 
     @GetMapping("/request/direction/{locID}")
-    public ResponseEntity<List<String>> getLocDirection(@PathVariable int locID, @RequestParam int startTime, @RequestParam int endTime)
-    {
+    public ResponseEntity<List<String>> getLocDirection(@PathVariable int locID, @RequestParam int startTime, @RequestParam int endTime) {
         List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.DIRECTION, locID, startTime, endTime);
         List<String> jsonRepresentation = valueList.stream()
                 .map(Value::toString)
@@ -38,19 +53,8 @@ public class ValueController
         return ResponseEntity.ok(jsonRepresentation);
     }
 
-    @GetMapping("/request/speed/{locID}")
-    public ResponseEntity<List<String>> getLocSpeedByNumber(@RequestParam int entries, @PathVariable int locID)
-    {
-        List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.SPEED, locID, entries);
-        List<String> jsonRepresentation = valueList.stream()
-                .map(Value::toString)
-                .toList();
-        return ResponseEntity.ok(jsonRepresentation);
-    }
-
     @GetMapping("/request/direction/number/{locID}")
-    public ResponseEntity<List<String>> getLocDirectionByNumber(@PathVariable int locID, @RequestParam int entries)
-    {
+    public ResponseEntity<List<String>> getLocDirectionByNumber(@PathVariable int locID, @RequestParam int entries) {
         List<Value> valueList = valueService.getLocValuesByScheme(LocValueScheme.DIRECTION, locID, entries);
         List<String> jsonRepresentation = valueList.stream()
                 .map(Value::toString)
